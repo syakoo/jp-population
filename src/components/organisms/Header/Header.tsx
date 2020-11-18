49c0117089898e0a725b941c897348d525c8f1c9
syakoo <nichijou4009@gmail.com>
2020-11-18 22:13:50 +0900

    add: styles to the Header component.

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,13 +1,22 @@
+import styled from 'styled-components'
 import React from 'react'
 
 // ____________________
 //
 const Header: React.FC = () => (
-  <header>
+  <_Header>
     <div>JP Population</div>
-  </header>
+  </_Header>
 )
 
 // ____________________
 //
+const _Header = styled.header`
+  background-color: ${(p) => p.theme.primary};
+  padding: 0.5em;
+  font-size: 1.5rem;
+  color: ${(p) => p.theme.white};
+  font-weight: bold;
+`
+
 export default Header
