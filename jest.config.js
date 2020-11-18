d03922913fbc4b303cd12eed21ee28b40a387903
syakoo <nichijou4009@gmail.com>
2020-11-18 23:03:36 +0900

    add: jest-transform-stub to jest.config.js.

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,14 @@ module.exports = {
     '^@src/(.+)': '<rootDir>/src/$1',
     '\\.scss$': '<rootDir>/node_modules/jest-css-modules',
   },
+  transform: {
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'jest-transform-stub',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   globals: {
     'ts-jest': {
-      tsConfig: {
+      tsconfig: {
         jsx: 'react',
       },
     },
