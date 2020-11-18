d5c8afcda9afecf9c982d771a9529fefa9100189
syakoo <sakodata0318@gmail.com>
2020-11-19 02:26:37 +0900

    update: PrefectureSelection comp to apply redux.

--- src/components/organisms/PrefectruesSelection/PrefectrueSelection.tsx
+++ src/components/organisms/PrefectruesSelection/PrefectrueSelection.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { ToggledDetails } from '@src/components/molecules/ToggledDetails'
+import { init, togglePref } from '@src/store/prefectures/prefecturesSlice'
 
 // ____________________
 //
 const PrefectureSelection: React.FC = () => {
+  const { prefectures } = useSelector((state) => state.prefectures)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (prefectures.length === 0) {
+      dispatch(init())
+    }
+  }, [])
+
   return (
     <ToggledDetails summary="設定">
-      <div>ここに県の選択</div>
+      {prefectures.map(({ prefCode, prefName, selected }) => (
+        <span key={prefCode}>
+          <label>
+            <input
+              type="checkbox"
+              checked={selected}
+              onChange={() => dispatch(togglePref({ prefCode }))}
+            />
+            {prefName}
+          </label>
+        </span>
+      ))}
     </ToggledDetails>
   )
 }
