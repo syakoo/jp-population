5995e299bbbdedcde3ab16b7ea4387d67ab13fd2
syakoo <nichijou4009@gmail.com>
2020-11-18 23:24:09 +0900

    add: Github Logo to Header.

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,11 +1,18 @@
 import styled from 'styled-components'
 import React from 'react'
 
+import ghLogo from '@src/assets/GitHub-Mark-32px.png'
+
 // ____________________
 //
 const Header: React.FC = () => (
   <_Header>
     <div>JP Population</div>
+    <div>
+      <a href="https://github.com/syakoo/jp-population">
+        <_GHImg src={ghLogo} alt="github-link" />
+      </a>
+    </div>
   </_Header>
 )
 
@@ -17,6 +24,14 @@ const _Header = styled.header`
   font-size: 1.5rem;
   color: ${(p) => p.theme.white};
   font-weight: bold;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const _GHImg = styled.img`
+  vertical-align: text-bottom;
+  cursor: pointer;
 `
 
 export default Header
