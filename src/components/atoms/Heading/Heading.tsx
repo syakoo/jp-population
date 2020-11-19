ee736b8c0b075e6afac4fcc3ac82d7471dff4db9
syakoo <sakodata0318@gmail.com>
2020-11-19 09:38:56 +0900

    update: change border-color of _H1.

--- src/components/atoms/Heading/Heading.tsx
+++ src/components/atoms/Heading/Heading.tsx
@@ -6,5 +6,5 @@ export const _H1 = styled.h1`
   font-size: 1.75em;
   font-weight: bold;
   color: ${(p) => p.theme.gray3};
-  border-bottom: 1px solid ${(p) => p.theme.gray2};
+  border-bottom: 1px solid ${(p) => p.theme.primary};
 `
