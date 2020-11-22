9c1bc1c04df1c9f090880093589d1f8294d9daad
syakoo <nichijou4009@gmail.com>
2020-11-22 09:26:55 +0900

    update: test for PopulationtransitionGraph.tsx.

--- src/components/organisms/PopulationTransitionGraph/PopulationTransitionGraph.test.tsx
+++ src/components/organisms/PopulationTransitionGraph/PopulationTransitionGraph.test.tsx
@@ -6,6 +6,23 @@ import PopulationTransitionGraph from './PopulationTransitionGraph'
 
 // ____________________
 //
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn((fn) => ({
+    graphDataValuePerYear: [
+      { 12: 3, 45: 6, year: 9 },
+      { 12: 9, 45: 3, year: 6 },
+    ],
+    selectedPrefectures: [
+      { prefCode: 12, prefName: 'PREF_12' },
+      { prefCode: 45, prefName: 'PREF_45' },
+    ],
+  })),
+}))
+
+// ____________________
+//
 describe('<PopulationTransitionGraph>', () => {
-  test.todo('It should be rendered without error.')
+  it('should render without error', async () => {
+    render(<PopulationTransitionGraph />)
+  })
 })
