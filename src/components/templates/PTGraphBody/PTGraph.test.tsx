fb1ccdafccd589b36f9623c99a6b98f1be414f33
syakoo <nichijou4009@gmail.com>
2020-11-23 16:41:25 +0900

    add: test.

--- src/components/templates/PTGraphBody/PTGraph.test.tsx
+++ src/components/templates/PTGraphBody/PTGraph.test.tsx
@@ -6,6 +6,27 @@ import PTGraphBody from './PTGraphBody'
 
 // ____________________
 //
+const mockDispatch = jest.fn()
+
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn((fn) => ({
+    graphDataValuePerYear: [
+      { 12: 3, 45: 6, year: 9 },
+      { 12: 9, 45: 3, year: 6 },
+    ],
+    selectedPrefectures: [
+      { prefCode: 12, prefName: 'PREF_12' },
+      { prefCode: 45, prefName: 'PREF_45' },
+    ],
+    prefectures: [],
+  })),
+  useDispatch: () => mockDispatch,
+}))
+
+// ____________________
+//
 describe('<PTGraph>', () => {
-  test.todo('It should be rendered without error.')
+  it('should be rendered without error', () => {
+    render(<PTGraphBody />)
+  })
 })
